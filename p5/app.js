263c2a05f0eb0042a35b4641aa8f43c4f63c3f83
agohorel <alex.oddlogic@gmail.com>
2019-01-10 16:39:06 -0500

    temporary p5 test sketch

--- p5/app.js
+++ p5/app.js
@@ -1,5 +1,10 @@
+p5.disableFriendlyErrors = true;
+
 let canvasDiv;
 let cnv;
+
+let flock = [];
+let num_boids = 15;
 
 function setup(){
 	canvasDiv = document.getElementById("p5-container");
@@ -7,16 +12,98 @@ function setup(){
 	let h = canvasDiv.offsetHeight;
 	cnv = createCanvas(w, h);
 	cnv.parent('p5-container');
+	background(0);
+	colorMode(HSB);
 }
 
 function draw(){
-	background(0);
-	fill(255, 0, 0);
-	rect(mouseX, mouseY, 50, 50);
+	if (flock.length < num_boids && frameCount % 2 === 0){
+		flock.push(new Boid());
+	}
+
+	for (let boid of flock){
+		boid.update();
+		boid.edges();
+		boid.lines(flock);
+	}
+
+	// screenshot and reset every ~30 seconds
+	// if (Math.floor(millis()) % 30000 <= 30){
+	// 	let timestamp = Date.now().toString().trim();
+	// 	saveCanvas(cnv, timestamp, "png");
+	// 	clear();
+	// 	background(0);
+	// 	flock = [];
+	// }
 }
 
 function windowResized() {
 	w = canvasDiv.offsetWidth;
 	h = canvasDiv.offsetHeight;
 	resizeCanvas(w, h);
+	background(0);
+}
+
+class Boid {
+	constructor(){
+		this.position = createVector(random(width), random(height));
+		this.radius = random(3, 6);
+		this.hue = random(0, 180);
+		this.saturation = random(0, 100);
+		this.brightness = random(0, 100);
+		this.velocity = p5.Vector.random2D();
+		this.velocity.setMag(random(.01, 5));
+		this.acceleration = createVector();
+		this.maxSpeed = 10;
+		this.perception = random(25, width/3);
+		this.thickness = random(this.radius*.15, this.radius*.5);
+		this.getsBrighter = true;
+		this.getsBigger = true;
+		this.maxRadius = random(6, 12);
+		this.minRadius = random(1, 3);
+	}
+
+	edges(){
+		if (this.position.x > width + this.radius){
+			this.position.x = 0;
+		} else if (this.position.x < -this.radius){
+			this.position.x = width;
+		}
+
+		if (this.position.y > height + this.radius){
+			this.position.y = 0;
+		} else if (this.position.y < -this.radius){
+			this.position.y = height;
+		}
+	}
+
+	lines(boids){
+		stroke(osc(this.hue, 360), osc(this.saturation, 100), osc(this.brightness, 100), osc(this.hue, 100));
+		strokeWeight(this.thickness);
+		let perceptionRadius = this.perception;
+		for (let other of boids){
+			let d = dist(this.position.x, this.position.y, other.position.x, other.position.y);
+			// check if surrounding boids are within "perceivable" range and that "other" is not "me"/"this"
+			if (d < perceptionRadius && d > perceptionRadius*.75 && other != this){
+				rotate(radians(osc(this.hue), 360));
+				line(this.position.x, this.position.y, other.position.x, other.position.y);
+			}
+		}
+	}
+
+	update(){
+		this.position.add(this.velocity);
+		this.velocity.add(this.acceleration);
+		this.velocity.limit(this.maxSpeed);
+		this.acceleration.set(0, 0); // reset acceleration after each update
+		this.radius = osc(this.hue, 2);
+		this.thickness = this.radius*.5;
+		this.hue += .0001;
+		this.saturation += random(.001, 1);
+		this.brightness += random(.001, 1);
+	}
+}
+
+function osc(angle, scalar){
+	return abs(sin(radians(angle)) * scalar);
 }
