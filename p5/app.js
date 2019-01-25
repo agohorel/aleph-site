d74a779f97fc37731dc5a50e2c69277d47555a33
agohorel <alex.oddlogic@gmail.com>
2019-01-25 18:43:28 -0500

    get local copy of song if available
    for localhost situations

--- p5/app.js
+++ p5/app.js
@@ -45,8 +45,7 @@ let img;
 let pastBuffer, padding, barWidth;
 
 function preload(){
-	// song = loadSound("../audio/song.mp3"); // 129 bpm
-	song = loadSound("https://raw.githubusercontent.com/agohorel/aleph-site/master/audio/song.mp3"); // 129 bpm
+	song = loadSound("../audio/song.mp3") || loadSound("https://raw.githubusercontent.com/agohorel/aleph-site/master/audio/song.mp3"); // 129 bpm
 }
 
 function setup(){
