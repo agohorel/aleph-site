a0828c6294e262041da0a0f81d380a696ca7a6c8
agohorel <alex.oddlogic@gmail.com>
2019-01-28 18:06:03 -0500

    email button exposes email form div

---
+++ js/index.js
@@ -0,0 +1,6 @@
+let emailBtn = document.querySelector(".social__icon--email");
+let emailFormDiv = document.querySelector(".section-subscribe");
+
+emailBtn.addEventListener("click", () => {
+	emailFormDiv.style.display = "block";
+});
