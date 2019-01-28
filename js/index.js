be6ce3ee226d19c212adc2974603f620df49bc85
agohorel <alex.oddlogic@gmail.com>
2019-01-28 18:25:46 -0500

    add working email form

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 let emailBtn = document.querySelector(".social__icon--email");
-let emailFormDiv = document.querySelector(".section-subscribe");
+let emailFormDiv = document.querySelector(".section-email");
 
 emailBtn.addEventListener("click", () => {
 	emailFormDiv.style.display = "block";
