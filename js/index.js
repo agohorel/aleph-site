26ec99bc1ea0daebb24e780884e5dc24ffe4cf63
agohorel <alex.oddlogic@gmail.com>
2019-01-28 19:36:36 -0500

    wrap social icon labels in a tags, fix footer margin

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
-let emailBtn = document.querySelector(".social__icon--email");
+let emailIcon = document.querySelector(".social__icon--email");
+let emailLink = document.querySelector("#email-text-link");
 let emailFormDiv = document.querySelector(".section-email");
 
-emailBtn.addEventListener("click", () => {
+emailIcon.addEventListener("click", () => {
+	emailFormDiv.style.display = "block";
+});
+
+emailLink.addEventListener("click", () => {
 	emailFormDiv.style.display = "block";
 });
