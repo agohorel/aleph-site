00cbdf8cef8b90eb910bf580ca699fa4c3cc3577
agohorel <alex.oddlogic@gmail.com>
2019-02-09 19:18:50 -0500

    hide downloads on mobile useragents, improve downloads responsiveness

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// show contact form
+
 let emailIcon = document.querySelector(".social__icon--email");
 let emailLink = document.querySelector("#email-text-link");
 let emailFormDiv = document.querySelector(".section-email");
@@ -9,3 +11,16 @@ emailIcon.addEventListener("click", () => {
 emailLink.addEventListener("click", () => {
 	emailFormDiv.style.display = "block";
 });
+
+// detect mobile
+
+let downloadsSection = document.querySelector(".section-downloads");
+let getStartedHref = document.querySelector(".btn-text");
+
+let isMobile = navigator.userAgent.toLowerCase().indexOf("mobile") !== -1 || 
+  			   navigator.userAgent.toLowerCase().indexOf("iphone") !== -1 || 
+  			   navigator.userAgent.toLowerCase().indexOf("android") !== -1 || 
+			   navigator.userAgent.toLowerCase().indexOf("windows phone") !== -1;
+
+isMobile ? downloadsSection.style.display = "none" : null; 
+isMobile ? getStartedHref.href = "#demonstration" : "#downloads"; 
